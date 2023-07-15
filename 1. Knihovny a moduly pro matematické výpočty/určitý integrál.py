f526dfce726086fa54e5e783b7829e7119fa779d
Pmk28 <127778553+Pmk28@users.noreply.github.com>
2023-07-15 11:46:08 +0200

    Update určitý integrál.py

--- 1. Knihovny a moduly pro matematické výpočty/určitý integrál.py
+++ 1. Knihovny a moduly pro matematické výpočty/určitý integrál.py
@@ -1,12 +1,32 @@
 from scipy import integrate
 import time as t
 
+def f(x):
+    return x**2
+
 def Scipy(a,b):
     start = t.time()
-    vysledek1 = integrate.quad(lambda x: x**2, a, b)
-    print(vysledek1)
+    for i in range(100):
+        vysledek = integrate.quad(lambda x: x**2, a, b)
     konec = t.time()
-    return konec - start
+    
+    return (konec - start,vysledek)
 
 vysledek = Scipy(0,1)
-print(f"Čas: {vysledek}")
+print(f"Scipy: {vysledek[0]}")
+
+
+a = 0
+b = 1
+n = 100000000 #počet subintervalů
+
+s = (b-a)/n #šířka každého intervalu
+integral=0
+start = t.time()
+for i in range(n):
+    x = a + i*s  # Aktuální hodnota x pro daný subinterval
+    integral += f(x)
+
+aproximace = integral * s
+konec = t.time()
+print(f"Bez Scipy:{konec - start}")
